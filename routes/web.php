847208bf7b1559045b0b100365667ae35e6beb58
Ian.H <ian@ianh.io>
2018-08-01 00:42:33 +0100

    Add more content to homepage
    Add goodybag bullet points.
    Add ‘sell for more’ section.
    Add ‘phones’ section.

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,42 @@ $app->group('', function() use ($app) {
         $device = new \stdClass();
         $device->isApple = $detectOs->is('MacOS');
 
+        // Goodybag bundle
         $heroGoodybag = new \stdClass();
         $heroGoodybag->sku = 'BD031';
+
+        $bundle = new \stdClass();
+        $bundle->hasMinutes = true;
+        $bundle->hasTexts = true;
+        $bundle->data = 'Always on*';
+        $bundle->minutes = 'Unlimited';
+        $bundle->texts = 'Unlimited';
+        $bundle->name = $heroGoodybag->sku;
+
+        // Phone model (hero) info from db
+        $phoneDetails = collect([
+            [
+                'make' => 'apple',
+                'model' => 'apple-iphone-se',
+                'title' => 'iPhone SE',
+                'color' => 'grey',
+                'price' => '8.64',
+            ],
+            [
+                'make' => 'apple',
+                'model' => 'apple-iphone-6',
+                'title' => 'iPhone 6',
+                'color' => 'grey',
+                'price' => '13.60',
+            ],
+            [
+                'make' => 'apple',
+                'model' => 'apple-iphone-7',
+                'title' => 'iPhone 7',
+                'color' => 'black',
+                'price' => '23.53',
+            ],
+        ]);
 
         return $this->view->render($response, 'home/index.twig', [
             'app' => $request->getAttribute('app'),
@@ -24,6 +58,9 @@ $app->group('', function() use ($app) {
             'flags' => new Collection($app->getContainer()['flags']),
             'device' => $device,
             'heroGoodybag' => $heroGoodybag,
+            'bundle' => $bundle,
+            'phonesPricesSmallPrint' => 'Based on £25 upfront payment and 24 month payment terms.',
+            'phoneDetails' => $phoneDetails,
         ]);
     })->setName('homepage');
 })->add(new \middleware\MasterLayout($app->getContainer()));
